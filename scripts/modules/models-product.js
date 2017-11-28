f5da1779da6e75703c9765b32b85e2dd365d26bc
ankur garha <speedyankur@gmail.com>
2017-11-28 18:40:42 +0530

    Family page changes

--- scripts/modules/models-product.js
+++ scripts/modules/models-product.js
@@ -12,8 +12,8 @@ define(["modules/jquery-mozu", "underscore", "modules/backbone-mozu", "hyprlive"
         return [zeroPad(date.getFullYear(), 4), zeroPad(date.getMonth() + 1, 2), zeroPad(date.getDate(), 2)].join('-');
     }
 
-
-    var ProductOption = Backbone.MozuModel.extend({
+    var ProductContent = Backbone.MozuModel.extend({}),
+    ProductOption = Backbone.MozuModel.extend({
         idAttribute: "attributeFQN",
         helpers: ['isChecked'],
         initialize: function() {
@@ -182,8 +182,298 @@ define(["modules/jquery-mozu", "underscore", "modules/backbone-mozu", "hyprlive"
             }
         }
     }),
+    FamilyItem = Backbone.MozuModel.extend({
+        mozuType: 'product',
+        idAttribute: 'productCode',
+        handlesMessages: true,
+        helpers: ['mainImage', 'notDoneConfiguring', 'hasPriceRange', 'supportsInStorePickup', 'isPurchasable','hasVolumePricing'],
+        defaults: {
+            purchasableState: {},
+            quantity: 1
+        },
+        dataTypes: {
+            quantity: Backbone.MozuModel.DataTypes.Int
+        },
+        validation: {
+            quantity: {
+                min: 1,
+                msg: Hypr.getLabel('enterProductQuantity')
+            }
+        },
+        relations: {
+            content: ProductContent,
+            price: PriceModels.ProductPrice,
+            priceRange: PriceModels.ProductPriceRange,
+            options: Backbone.Collection.extend({
+                model: ProductOption
+            })
+        },
+        getBundledProductProperties: function(opts) {
+            var self = this,
+                loud = !opts || !opts.silent;
+            if (loud) {
+                this.isLoading(true);
+                this.trigger('request');
+            }
 
-    ProductContent = Backbone.MozuModel.extend({}),
+            var bundledProducts = this.get('bundledProducts'),
+                numReqs = bundledProducts.length,
+                deferred = api.defer();
+            _.each(bundledProducts, function(bp) {
+                var op = api.get('product', bp.productCode);
+                op.ensure(function() {
+                    if (--numReqs === 0) {
+                        _.defer(function() {
+                            self.set('bundledProducts', bundledProducts);
+                            if (loud) {
+                                this.trigger('sync', bundledProducts);
+                                this.isLoading(false);
+                            }
+                            deferred.resolve(bundledProducts);
+                        });
+                    }
+                });
+                op.then(function(p) {
+                    _.each(p.prop('properties'), function(prop) {
+                        if (!prop.values || prop.values.length === 0 || prop.values[0].value === '' || prop.values[0].stringValue === '') {
+                            prop.isEmpty = true;
+                        }
+                    });
+                    _.extend(bp, p.data);
+                });
+            });
+
+            return deferred.promise;
+        },
+        hasPriceRange: function() {
+            return this._hasPriceRange;
+        },
+        hasVolumePricing: function() {
+            return this._hasVolumePricing;
+        },
+        calculateHasPriceRange: function(json) {
+            this._hasPriceRange = json && !!json.priceRange;
+        },
+        initialize: function(conf) {
+            var me = this;
+            setTimeout(function(){
+
+
+                me.apiGet().then(function(){
+                    var slug = me.get('content').get('seoFriendlyUrl');
+
+                    _.bindAll(me, 'calculateHasPriceRange', 'onOptionChange');
+                    me.listenTo(me.get("options"), "optionchange", me.onOptionChange);
+                    me._hasVolumePricing = false;
+                    me._minQty = 1;
+                    if (me.get('volumePriceBands') && me.get('volumePriceBands').length > 0) {
+                        me._hasVolumePricing = true;
+                        me._minQty = _.first(me.get('volumePriceBands')).minQty;
+                        if (me._minQty > 1) {
+                            if (me.get('quantity') <= 1) {
+                                me.set('quantity', me._minQty);
+                            }
+                            me.validation.quantity.msg = Hypr.getLabel('enterMinProductQuantity', me._minQty);
+                        }
+                    }
+                    me.updateConfiguration = _.debounce(me.updateConfiguration, 300);
+                    me.set({ url: (HyprLiveContext.locals.siteContext.siteSubdirectory || '') + (slug ? "/" + slug : "") +  "/p/" + me.get("productCode")});
+                    me.lastConfiguration = [];
+                    me.calculateHasPriceRange(conf);
+                    me.on('sync', me.calculateHasPriceRange);  
+                    var variations = me.get('variations');
+                    var variationCodes = me.get('variationCodes');
+                    var variation_pro = [];
+                    var options_arr = [];
+                    me.set('variations', variation_pro);
+                    _.each(variations, function(valued) {
+                        for (var k = 0; k < variationCodes.length; k++) {
+                            if (valued.productCode === variationCodes[k]) {
+                                variation_pro.push(valued);
+                                //variation_pro = _.uniq(variation_pro);
+                                if (me.attributes.options) {
+                                    for (var l = 0; l < valued.options.length; l++) {
+                                        options_arr.push(valued.options[l].valueSequence);
+                                        options_arr = _.uniq(options_arr); 
+                                    }
+                                }
+                            }
+                        }
+                    });
+                    me.set('variations', variation_pro);
+                    //remove options
+                    var byIDVal = JSON.parse(JSON.stringify(me.get('options')._byId));
+                    for(var i=0;i<me.get("options").models.length;i++){
+                        var opt_pro= [];
+                        var option = me.get("options").models[i];
+                        var key = option.get("attributeFQN");
+                        for (var b = 0; b < option.get('values').length; b++) {
+                            for (var c = 0; c < options_arr.length; c++) {
+                                if (option.get('values')[b].attributeValueId === options_arr[c]) {
+                                    opt_pro.push(option.get('values')[b]);     
+                                    break;                       
+                                }
+                            }
+                        } 
+                        me.get('options').get(key).set('values', opt_pro);                       
+                    }
+                });
+            },1000);
+
+        },
+        mainImage: function() {
+            var productImages = this.get('content.productImages');
+            return productImages && productImages[0];
+        },
+        notDoneConfiguring: function() {
+            return this.get('productUsage') === Product.Constants.ProductUsage.Configurable && !this.get('variationProductCode');
+        },
+        isPurchasable: function() {
+            var purchaseState = this.get('purchasableState');
+            if (purchaseState.isPurchasable){
+                return true;
+            }
+            if (this._hasVolumePricing && purchaseState.messages && purchaseState.messages.length === 1 && purchaseState.messages[0].validationType === 'MinQtyNotMet') {
+                return true;
+            }
+            return false;
+        },
+        supportsInStorePickup: function() {
+            return _.contains(this.get('fulfillmentTypesSupported'), Product.Constants.FulfillmentTypes.IN_STORE_PICKUP);
+        },
+        getConfiguredOptions: function(options) {
+            return this.get('options').reduce(function(biscuit, opt) {
+                opt.addConfiguration(biscuit, options);
+                return biscuit;
+            }, []);
+        },
+
+
+        addToCart: function () {
+            var me = this;
+            this.whenReady(function () {
+                if (!me.validate()) {
+                    var fulfillMethod = me.get('fulfillmentMethod');
+                    if (!fulfillMethod) {
+                        fulfillMethod = (me.get('goodsType') === 'Physical') ? Product.Constants.FulfillmentMethods.SHIP : Product.Constants.FulfillmentMethods.DIGITAL;
+                    }
+                    me.apiAddToCart({
+                        options: me.getConfiguredOptions(),
+                        fulfillmentMethod: fulfillMethod,
+                        quantity: me.get("quantity")
+                    }).then(function (item) {
+                        me.trigger('addedtocart', item);
+                    });
+                }
+            });
+        },
+        addToWishlist: function() {
+            var me = this;
+            this.whenReady(function() {
+                if (!me.validate()) {
+                    me.apiAddToWishlist({
+                        customerAccountId: require.mozuData('user').accountId,
+                        quantity: me.get("quantity"),
+                        options: me.getConfiguredOptions()
+                    }).then(function(item) {
+                        me.trigger('addedtowishlist', item);
+                    });
+                }
+            });
+        },
+        addToCartForPickup: function(locationCode, locationName, quantity) {
+            var me = this;
+            this.whenReady(function() {
+                return me.apiAddToCartForPickup({
+                    fulfillmentLocationCode: locationCode,
+                    fulfillmentMethod: Product.Constants.FulfillmentMethods.PICKUP,
+                    fulfillmentLocationName: locationName,
+                    quantity: quantity || 1
+                }).then(function(item) {
+                    me.trigger('addedtocart', item);
+                });
+            });
+        },
+        onOptionChange: function() {
+            this.isLoading(true);
+            this.updateConfiguration();
+        },
+        updateQuantity: function (newQty) {
+            if (this.get('quantity') === newQty) return;
+            this.set('quantity', newQty);
+            if (!this._hasVolumePricing) return;
+            if (newQty < this._minQty) {
+                return this.showBelowQuantityWarning();
+            }
+            this.isLoading(true);
+            this.apiConfigure({ options: this.getConfiguredOptions() }, { useExistingInstances: true });
+        },
+        showBelowQuantityWarning: function () {
+            this.validation.quantity.min = this._minQty;
+            this.validate();
+            this.validation.quantity.min = 1;
+        },
+        handleMixedVolumePricingTransitions: function (data) {
+            if (!data || !data.volumePriceBands || data.volumePriceBands.length === 0) return;
+            if (this._minQty === data.volumePriceBands[0].minQty) return;
+            this._minQty = data.volumePriceBands[0].minQty;
+            this.validation.quantity.msg = Hypr.getLabel('enterMinProductQuantity', this._minQty);
+            if (this.get('quantity') < this._minQty) {
+                this.updateQuantity(this._minQty);
+            }
+        },
+        updateConfiguration: function() {
+            var me = this,
+              newConfiguration = this.getConfiguredOptions();
+            if (JSON.stringify(this.lastConfiguration) !== JSON.stringify(newConfiguration)) {
+                this.lastConfiguration = newConfiguration;
+                this.apiConfigure({ options: newConfiguration }, { useExistingInstances: true })
+                    .then(function (apiModel) {
+                        if (me._hasVolumePricing) {
+                            me.handleMixedVolumePricingTransitions(apiModel.data);
+                        }
+                     });
+            } else {
+                this.isLoading(false);
+            }
+        },
+        parse: function(prodJSON) {
+            if (prodJSON && prodJSON.productCode && !prodJSON.variationProductCode) {
+                this.unset('variationProductCode');
+            }
+            return prodJSON;
+        },
+        toJSON: function(options) {
+            var j = Backbone.MozuModel.prototype.toJSON.apply(this, arguments);
+            if (!options || !options.helpers) {
+                j.options = this.getConfiguredOptions({ unabridged: true });
+            }
+            if (options && options.helpers) {
+                if (typeof j.mfgPartNumber == "string") j.mfgPartNumber = [j.mfgPartNumber];
+                if (typeof j.upc == "string") j.upc = [j.upc];
+                if (j.bundledProducts && j.bundledProducts.length === 0) delete j.bundledProducts;
+            }
+            return j;
+        }
+    }, {
+        Constants: {
+            FulfillmentMethods: {
+                SHIP: "Ship",
+                PICKUP: "Pickup",
+                DIGITAL: "Digital"
+            },
+            // for catalog instead of commerce
+            FulfillmentTypes: {
+                IN_STORE_PICKUP: "InStorePickup"
+            },
+            ProductUsage: {
+                Configurable: 'Configurable'
+            }
+        }       
+    }),
+    FamilyProducts = FamilyItem.extend({}),
+
+
 
     Product = Backbone.MozuModel.extend({
         mozuType: 'product',
@@ -209,6 +499,9 @@ define(["modules/jquery-mozu", "underscore", "modules/backbone-mozu", "hyprlive"
             priceRange: PriceModels.ProductPriceRange,
             options: Backbone.Collection.extend({
                 model: ProductOption
+            }),
+            family:Backbone.Collection.extend({
+                model: FamilyProducts
             })
         },
         getBundledProductProperties: function(opts) {
@@ -427,8 +720,17 @@ define(["modules/jquery-mozu", "underscore", "modules/backbone-mozu", "hyprlive"
             ProductUsage: {
                 Configurable: 'Configurable'
             }
-        }
+        },
+        fromCurrent: function () {
+            var data = require.mozuData(this.prototype.mozuType);
+            var families = _.find(data.properties, function(e) {
+                return e.attributeFQN === "tenant~family-members" && e.values;
+            });
+            data.family = JSON.parse(families.values[0].stringValue);
+            return new this(data, { silent: true, parse: true });
+        }        
     }),
+
 
     ProductCollection = Backbone.MozuModel.extend({
         relations: {
