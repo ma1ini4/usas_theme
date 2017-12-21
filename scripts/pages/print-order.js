e82016cbb0fff1d0a160eaf628d6420f1e492751
Rajesh Kumar <rajesh.kumar@trantorinc.com>
2017-12-21 12:20:55 +0530

    Print order

--- scripts/pages/print-order.js
+++ scripts/pages/print-order.js
@@ -1,17 +1,38 @@
 require(["modules/jquery-mozu", "hyprlive", "modules/backbone-mozu", "modules/api"], function($, Hypr, Backbone, api) {
-    $(document).ready(function() {
+    var PrintOrderView = Backbone.MozuView.extend({
+        templateName: 'modules/my-account/print-order',
+        events: {
+          'click [data-mz-print-order]' : 'printOrder'
+        },
+        printOrder: function(){
+          window.print();
+        },
+        render: function() {
+          Backbone.MozuView.prototype.render.apply(this);
+        }
+    });
+
+    var printViewModel = Backbone.Model.extend();
+
+    $(document).ready(function(){
         var custid = require.mozuData('user');
-            var ordid = location.hash.substring(1);
-            var categorydetailsurl = '/api/commerce/orders/' + ordid;
-            api.request('GET', categorydetailsurl).then(function(resp) {
-                var cmpdet = {
-                    "name": document.getElementById('companyname').value,
-                    "add": document.getElementById('companyaddr').value,
-                    "url": document.getElementById('company-url').value
-                };
-                resp.cmp = cmpdet;
-                $('#print').empty();
-                $('#print').append(Hypr.getTemplate('modules/my-account/print-order').render({ model: resp }));
+        var ordid = location.hash.substring(1);
+        var categorydetailsurl = '/api/commerce/orders/' + ordid;
+        api.request('GET', categorydetailsurl).then(function(resp) {
+            var cmpdet = {
+                "name": document.getElementById('companyname').value,
+                "add": document.getElementById('companyaddr').value,
+                "url": document.getElementById('company-url').value
+            };
+
+            resp.cmp = cmpdet;
+            document.getElementById("print").style.display = "block";
+            var printModel = new printViewModel(resp);
+            var printOrderView = new PrintOrderView({
+                el: $('#print'),
+                model: printModel
             });
+            printOrderView.render(); 
+        });
     });
 });
