0c56ebb75d5cc418b022b557ced387f43ac22f7c
Rajesh Kumar <rajesh.kumar@trantorinc.com>
2018-01-02 10:59:13 +0530

    print order

--- scripts/pages/print-order.js
+++ scripts/pages/print-order.js
@@ -1,38 +1,40 @@
 require(["modules/jquery-mozu", "hyprlive", "modules/backbone-mozu", "modules/api"], function($, Hypr, Backbone, api) {
     var PrintOrderView = Backbone.MozuView.extend({
         templateName: 'modules/my-account/print-order',
-        events: {
-          'click [data-mz-print-order]' : 'printOrder'
-        },
         printOrder: function(){
           window.print();
+        },
+        initialize : function(){
+            var self = this;
+            console.log("HERE");
+            var custid = require.mozuData('user');
+            var ordid = location.hash.substring(1);
+            var categorydetailsurl = '/api/commerce/orders/' + ordid;
+            api.request('GET', categorydetailsurl).then(function(resp) {
+                var cmpdet = {
+                    "name": document.getElementById('companyname').value,
+                    "add": document.getElementById('companyaddr').value,
+                    "url": document.getElementById('company-url').value
+                };
+                resp.cmp = cmpdet;
+                console.log("resp", resp);
+                self.model.set(resp);
+                window.view.render();
+            });
         },
         render: function() {
           Backbone.MozuView.prototype.render.apply(this);
         }
     });
 
-    var printViewModel = Backbone.Model.extend();
-
     $(document).ready(function(){
-        var custid = require.mozuData('user');
-        var ordid = location.hash.substring(1);
-        var categorydetailsurl = '/api/commerce/orders/' + ordid;
-        api.request('GET', categorydetailsurl).then(function(resp) {
-            var cmpdet = {
-                "name": document.getElementById('companyname').value,
-                "add": document.getElementById('companyaddr').value,
-                "url": document.getElementById('company-url').value
-            };
-
-            resp.cmp = cmpdet;
+            var printViewModel = Backbone.MozuModel.extend();
             document.getElementById("print").style.display = "block";
-            var printModel = new printViewModel(resp);
-            var printOrderView = new PrintOrderView({
+            var printModel = new printViewModel();
+            var printOrderView = window.view = new PrintOrderView({
                 el: $('#print'),
                 model: printModel
             });
             printOrderView.render(); 
-        });
     });
 });
