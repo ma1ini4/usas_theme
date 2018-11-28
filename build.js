4f5b9d228d1a2e7035699af99be1c4dde923cf62
naminder-trantor <naminder.singh@trantorinc.com>
2018-11-28 21:47:25 +0530

    Added Slick plugin

--- build.js
+++ build.js
@@ -12,7 +12,8 @@
         elevatezoom: "vendor/jquery-elevatezoom/jquery.elevatezoom.min",
         doubletaptogo:"vendor/jquery-doubleTapToGo/jquery.doubleTapToGo.min",
         doubletaptogoipad:"vendor/jquery-doubleTapToGo/jquery.doubleTapToGoIpad.min",
-        async: "vendor/async/async.min"
+        async: "vendor/async/async.min",
+        slick: "vendor/slick/slick.min"
     },
     dir: "compiled/scripts/",
     locale: "en-us",
