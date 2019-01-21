ed2bceb9626270229d92866bf0b3276d7bc3c5dd
mkaur23 <manpreet.kaur2@kibocommerce.com>
2019-01-22 00:06:36 +0530

    Implementation Slick Slider (#230)
    * Implemented slick slider

--- scripts/widgets/rti/rti-rec-script.js
+++ scripts/widgets/rti/rti-rec-script.js
@@ -7,10 +7,11 @@ require([
         'modules/backbone-mozu',
         'modules/models-product',
         'widgets/rti/recommended-products',
-        'bxslider'
+        'bxslider',
+        'slick'
         //'vendor/jquery/jquery-ui'
     ],
-    function($, Hypr, HyprLiveContext, _, api, Backbone, ProductModels, RecommendedProducts, bxslider) {
+    function($, Hypr, HyprLiveContext, _, api, Backbone, ProductModels, RecommendedProducts, bxslider, slickSlider) {
 
         // rtiOptions will contain variables used by the
         //whole page. They can be set in every widget editor, but only the first
@@ -310,14 +311,157 @@ require([
             });
         };
 
-        try {
-            var productInstance = RecommendedProducts.getInstance(rtiOptions);
-            productInstance.getProductData(function(data) {
-                renderData(data);
+        var globalRenderData = function (data) {
+            $('#global-cart .recommended-product-container').each(function () {
+                var container = $(this);
+                var placeholder = globalConfig.placeholder;
+                var numberOfItems = globalConfig.quantity || globalConfig.numberOfItems;
+                var configTitle = globalConfig.title;
+                var format = globalConfig.format;
+                if (pageContext.isEditMode) {
+                    container.text('<b>Here Goes your RTI Recommended items</b>');
+                    return;
+                }
+                /*
+                Our data will contain information about lots of different possible widgets.
+                First we want to reduce that data to only the placeholderName we're dealing with.
+                */
+                var currentProducts = $.grep(data, function (e) {
+                    return e.placeholderName == placeholder;
+                });
+                /*
+                We should at this point have a list of results with the correct placeholderName,
+                and that last should only be 1 item long.
+                If that first item doesn't exist, there was a problem.
+                */
+                if (!currentProducts[0]) {
+                    if (pageContext.isEditMode) {
+                        /*
+                        If we reach this point, it means there wasn't a placeholderName in the
+                        data that was returned that matches the one we selected.
+                        */
+                        container.text("Placeholder not found.");
+                    }
+                } else {
+                    //We have the data for our widget now. Time to fill it up.
+                    var displayName;
+                    //if configTitle has a value, the user entered a title to
+                    //override the title set in RTI.
+                    if (configTitle) {
+                        displayName = configTitle;
+                    } else {
+                        //if configTitle has no value, we get the title from the
+                        //product results call
+                        displayName = currentProducts[0].displayName;
+                    }
+
+                    //We slice the productList we received according to the limit set
+                    //in the editor
+                    var productList;
+                    if (currentProducts[0].productList.length > numberOfItems) {
+                        productList = currentProducts[0].productList.slice(0, numberOfItems);
+                    } else {
+                        productList = currentProducts[0].productList;
+                    }
+
+                    //Turns list of product IDs into a product collection
+                    getMozuProducts(productList).then(function (products) {
+                        if (products.length !== 0) {
+                            var productsByRank = _.sortBy(products, 'rtiRank');
+                            productList = productsByRank;
+                            var prodColl = new ProductModels.ProductCollection();
+                            prodColl.set('items', productList);
+                            prodColl.set('bnData', data.bnData);
+
+                            //Time to actually render
+
+                            if (currentProducts[0].editModeMessage) {
+                                if (pageContext.isEditMode) {
+                                    container.text(currentProducts[0].editModeMessage);
+                                }
+                            } else {
+                                container.find('.mz-related-products.hidden-print').html('<h3 class="' + placeholder + ' slider-title"><span>' + displayName + '</span></h3>');
+                                if (!format) {
+                                    format = "carousel";
+                                }
+                                if (format == "carousel") {
+                                    var productListView = new ProductListView({
+                                        el: container.find('.rti-recommended-products'),
+                                        model: prodColl
+                                    });
+                                    try {
+                                        container.find('.rti-recommended-products .bxslider').unslick();
+                                    } catch (e) { }
+                                    productListView.render();
+                                    if (productList.length > 1) {
+                                        $("." + placeholder + '.rti-recommended-products .bxslider').slick({
+                                            slidesToShow: 3,
+                                            slidesToScroll: 1,
+                                            infinite: false,
+                                            prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+                                            nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+                                        });
+                                    }
+                                    if (productList.length === 0) {
+                                        container.find('.recommended-product-container').hide();
+                                    }
+                                    return;
+                                }
+                            }
+                        } else {
+                            if (pageContext.isEditMode) {
+                                container.text("There was a problem retrieving products from your catalog that match the products received from RTI.");
+                            }
+                        }
+                    });
+                }
             });
+        };
+        
+        try {
+            if ($('.recommended-product-container').not('#global-cart .recommended-product-container').length) {
+                var productInstance = RecommendedProducts.getInstance(rtiOptions);
+                productInstance.getProductData(function (data) {
+                    renderData(data);
+                });
+            }
         } catch (err) {
             //console.log(err);
         }
         /*Recommended Product Code Ends*/
+        /* Code for Global Cart*/
+        var globalDisplay;
+        var globalConfig;
+        var globalRtiOptions;
+        if ($('#global-cart .recommended-product-container').length) {
+            globalDisplay = $('#global-cart .recommended-product-container').first();
+            globalConfig = globalDisplay.data('mzRtiRecommendedProducts');
+            globalRtiOptions = {
+                customerId: globalConfig.customerId || "",
+                customerCode: globalConfig.customerCode || "",
+                pageType: globalConfig.pageType || "",
+                jsInject: globalConfig.javascriptInjection || "",
+                includeSiteId: globalConfig.includeSiteId || false,
+                includeTenantId: globalConfig.includeTenantId || false
+            };
+        }
+
+        // function updateGCRTI() {
+        //     if ($('#global-cart .recommended-product-container').length) {
+        //         try {
+        //             var GCProductInstance = GCRecommendedProducts.getInstance(globalRtiOptions);
+        //             GCProductInstance.getProductData(function (data) {
+        //                 globalRenderData(data);
+        //             });
+        //         } catch (err) {
+        //             //
+        //         }
+        //     }
+        // }
+        // window.updateGCRTI = updateGCRTI;
+        var globalCartElement = document.getElementsByClassName("ml-header-global-cart-wrapper");
+        globalCartElement[0].addEventListener("mouseenter", function (event) {
+            $('#global-cart-rti .bxslider')[0].slick.refresh();
+        }, false);
 
     });
