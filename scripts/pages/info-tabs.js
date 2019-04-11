778a65c28709a465af16ff18b81ae4d284fb0c61
Demis <demis@deplabs.com>
2019-04-11 09:58:04 -0700

    USAS-295: Implement Info Tabs theme extension for PDP

---
+++ scripts/pages/info-tabs.js
@@ -0,0 +1,35 @@
+﻿require([
+    "modules/jquery-mozu",
+    "underscore",
+    "hyprlive",
+    "modules/backbone-mozu",
+    "modules/models-product",
+    "hyprlivecontext"
+], function($, _, Hypr, Backbone, ProductModels, HyprLiveContext) {
+
+    var InfoTabsView = Backbone.MozuView.extend({
+        templateName: 'modules/product/product-info-tabs',
+        render: function() {
+            Backbone.MozuView.prototype.render.call(this);
+        }
+    });
+
+    $(document).ready(function() {
+
+        var productInfoTab = ProductModels.Product.fromCurrent();
+        var infoTabsView = new InfoTabsView({
+            el: $('.info-tabs-details'),
+            model: productInfoTab
+        });
+
+        window.infoTabsView = infoTabsView;
+        productInfoTab.processInfoTabsContent();
+        infoTabsView.render();
+
+        window.infoTabsView.on('infoTabsCollectionViewReady', function() {
+            this.render();
+        });
+
+    });
+
+});
