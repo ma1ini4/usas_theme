b359e64a3bf9809466c6ef308873e335c1929e55
Vyacheslav Goncharuk <vyacheslav.goncharuk@deplabs.com>
2019-06-03 19:09:28 +0300

    DEPINT-165 : Email form : ADD : Email form with AWS

---
+++ scripts/widgets/sendEmail/sendemail.js
@@ -0,0 +1,65 @@
+require(["modules/jquery-mozu", "hyprlive"],
+	function ($, Hypr) {
+        $(document).ready(function () {
+            var mozuFullConfig = require.mozuData("config");
+            var config = require.mozuData("config").config;
+            var formId = config.customId || mozuFullConfig.definitionId;
+
+            // body params
+            var sourceEmail = config.email_from ? config.email_from : "system@deplabs.com",
+                ccEmailAddresses = config.email_cc ? config.email_cc.split(',') : [],
+                subjectdata = config.subject ? config.subject : "Empty subject",
+                emailTemplate = config.form_email_template ? config.form_email_template : "Empty text";
+
+            // this will look for a `data-custom-attribute` as name and get #id 
+            // element values and substitute to the form
+            $('#'+formId+' input[data-custom-attribute]').each(function(idx, el) {
+                el.value = $('#'+el.name).val();
+            });
+
+            $('input[name="submit-email-form"]').on('click', function(e) {
+                e.preventDefault();
+
+                var replacedTemplate = emailTemplate;
+                var formSerialize = $('#'+formId).serialize(),
+                    toEmailAddresses = new Array($('#'+formId+' input[name="email"]').val()),
+                    formArray = $('#'+formId).serializeArray();
+                
+                // recoursively replace {%value%} with needed values
+                formArray.forEach(function(el, idx) {
+                    var field = el.name;
+                    var value = el.value;
+                    console.log('value', value);
+                    if (value && value !== '') {
+                        var regex = new RegExp("{"+field+"}", "gi");
+                        replacedTemplate = replacedTemplate.replace(regex, value);
+                    }
+                });
+
+                // form the data body
+                var body = {
+                    "bccEmailAddresses": [],
+                    "ccEmailAddresses": [],
+                    "toEmailAddresses": toEmailAddresses,
+                    "bodyData": replacedTemplate ? replacedTemplate : emailTemplate,
+                    "bodyCharset": "UTF-8",
+                    "subjectdata": subjectdata,
+                    "subjectCharset": "UTF-8",
+                    "sourceEmail": sourceEmail,
+                    "replyToAddresses": [sourceEmail]
+                };
+
+                $.ajax({
+                    url: 'https://5htr6ylbhi.execute-api.us-east-1.amazonaws.com/development/sendMail', // TODO: transfer to ARC
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json" // welp, that was the game changer in this request
+                    },
+                    data: JSON.stringify(body)
+                })
+                .success(function(response){
+                    console.log(response);
+                });
+            });
+        });
+});
