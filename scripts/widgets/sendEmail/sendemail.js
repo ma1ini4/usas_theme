df74461847ea72f0f36ab6c5e6ddb9eb99539f1b
Vyacheslav Goncharuk <vyacheslav.goncharuk@deplabs.com>
2019-06-05 20:32:53 +0300

    DEPINT-165 : Email form : ADD : Finilizing steps made (popups + fixes)

--- scripts/widgets/sendEmail/sendemail.js
+++ scripts/widgets/sendEmail/sendemail.js
@@ -9,12 +9,21 @@ require(["modules/jquery-mozu", "hyprlive"],
             var sourceEmail = config.email_from ? config.email_from : "system@deplabs.com",
                 ccEmailAddresses = config.email_cc ? config.email_cc.split(',') : [],
                 subjectdata = config.subject ? config.subject : "Empty subject",
-                emailTemplate = config.form_email_template ? config.form_email_template : "Empty text";
+                emailTemplate = config.form_email_template ? config.form_email_template : "Empty text",
+                successMessage = config.form_success_message ? config.form_success_message : "Mail sent successfully",
+                errorMessage = config.form_error_message ? config.form_error_message : "Error sending email";
 
             // this will look for a `data-custom-attribute` as name and get #id 
             // element values and substitute to the form
             $('#'+formId+' input[data-custom-attribute]').each(function(idx, el) {
                 el.value = $('#'+el.name).val();
+            });
+
+            $('.close').on('click', function() {
+                $('.popup-overlay').fadeOut(200);
+            });
+            $('body').on('click', function() {
+                $('.popup-overlay').fadeOut(200);
             });
 
             $('input[name="submit-email-form"]').on('click', function(e) {
@@ -56,11 +65,20 @@ require(["modules/jquery-mozu", "hyprlive"],
                     method: 'POST',
                     headers: {
                         "Content-Type": "application/json", // welp, that was the game changer in this request
-                        "data": JSON.stringify(body)
+                        "data": JSON.stringify(body),
+                        "successMessage": successMessage,
+                        "errorMessage": errorMessage
                     }
                 })
                 .success(function(response){
-                    console.log(response);
+                    var message = response.message;
+                    if (response.statusCode == 200) {
+                        $('.email-message').html(message);
+                        $('.popup-overlay').fadeIn(200);
+                    } else {
+                        $('.email-message').html(message);
+                        $('.popup-overlay').fadeIn(200);
+                    }
                 });
             });
         });
