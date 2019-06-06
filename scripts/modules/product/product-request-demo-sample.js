351555a263d3892ada8c300f3160c85d53993061
Vlad Duma <vlad.duma@deplabs.com>
2019-06-06 15:42:31 +0300

    USAS-380 : DemoSample : UPDATE : Widget configured and set

--- scripts/modules/product/product-request-demo-sample.js
+++ scripts/modules/product/product-request-demo-sample.js
@@ -1,33 +1,36 @@
 require(["modules/jquery-mozu", "hyprlive"], function ($, Hypr) {
 
+    
     $(document).ready(function(){
         
         $('#product-request-demo').on('click', function (e) {
             e.preventDefault();
-
+            
             var requestType = "demo";
+            var demoEmail = Hypr.getThemeSetting('requestDemoEmail');
 
-            sendRequest(requestType);
+            sendRequest(requestType, demoEmail);
         });
         
         $('#product-request-sample').on('click', function (e) {
             e.preventDefault();
-
+            
             var requestType = "sample";
-
-            sendRequest(requestType);
+            var sampleEmail = Hypr.getThemeSetting('requestSampleEmail');
+            
+            sendRequest(requestType, sampleEmail);
         });
     });
 
-    function sendRequest(type){
+    function sendRequest(type, email){
         $('input[data-custom-attribute="demo-sample"]').val(type);
+        $('input[data-custom-attribute="form-email"]').val(email);
+
 
         $('input[name="submit-email-form"]').click();
 
-        $('#demo-sample-container .secondary-btn').hide();
+        // $('#demo-sample-container .secondary-btn').hide();
 
-        $('#demo-sample-container').html('<p class="tab-content-request-text">You have successfully requested a ' + type + '!</p>');
-
-        console.log('sent successfully');        
+        // $('#demo-sample-container').html('<p class="tab-content-request-text">You have successfully requested a ' + type + '!</p>');
     }
 });
