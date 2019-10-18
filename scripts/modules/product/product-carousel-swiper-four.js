3539fca65be90aa7242e3401c9d0ce828e926bce
Vlad Duma <vlad.duma@deplabs.com>
2019-10-18 15:08:23 +0300

    USASNGI-522 : RTI slider : UPDATE : Disable slider if not enough slides

--- scripts/modules/product/product-carousel-swiper-four.js
+++ scripts/modules/product/product-carousel-swiper-four.js
@@ -1,5 +1,14 @@
 define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>jQuery', 'swiper'], function($, Swiper) {
-    var swiper = new Swiper('#product-carousel-slider.swiper-container-4', {
+ var swiper;
+  $(document).ready(function() {
+    destroySlider();
+  });
+  $(window).resize(function() {
+    destroySlider();
+  });
+
+  function createSlider() {
+      swiper = new Swiper('#product-carousel-slider.swiper-container-4', {
         slidesPerView: 6,
         spaceBetween: 20,
         loop: true,
@@ -10,22 +19,56 @@ define(['shim!vendor/typeahead.js/typeahead.bundle[modules/jquery-mozu=jQuery]>j
         preventClicks: false,
         preventClicksPropagation: false,
         breakpoints: {
-            1024: {
-                slidesPerView: 5,
-                spaceBetween: 20
-              },
-              768: {
-                slidesPerView: 4,
-                spaceBetween: 15
-              },
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 10
-              },
-              320: {
-                slidesPerView: 1,
-                spaceBetween: 10
-            }
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 20
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 15
+          },
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          }
         }
-    });
+      });
+  }
+  
+  function destroySlider() {
+    if ($('#product-carousel-slider').data('total-items') <= getMinSlideCount()) {
+      if(swiper) {
+        swiper.destroy(true, true);
+      }
+      $('#product-carousel-slider').addClass('no-swiper');
+      $('.swiper-button-next, .swiper-button-prev').hide();
+    } else {
+      createSlider();
+      $('#product-carousel-slider').removeClass('no-swiper');
+      $('.swiper-button-next, .swiper-button-prev').show();
+    }
+  }
+
+  function getMinSlideCount() {
+    var width = $(window).width(),
+        minSlides;
+
+    if(width > 1024) {
+      minSlides = 6;
+    } else if (width <=1024 && width > 768) {
+      minSlides = 4;
+    } else if (width <= 768 && width > 640) {
+      minSlides = 3;
+    } else if (width <= 640 && width > 320) {
+      minSlides = 2;
+    } else if (width <= 320) {
+      minSlides = 1;
+    }
+    
+    return minSlides;
+  }
 });
