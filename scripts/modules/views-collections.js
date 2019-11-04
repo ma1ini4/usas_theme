7a2b861df4b5c01e48c9ef48075839b899b7ea6b
Vlad Duma <vlad.duma@deplabs.com>
2019-11-04 18:56:33 +0200

    USASNGI-627 :  Infinite scroller : FIX : Remove preloader on selected click

--- scripts/modules/views-collections.js
+++ scripts/modules/views-collections.js
@@ -370,7 +370,14 @@ define([
         }
         if ($(".view-all.selected").length) {
             InfiniteScroller.update();
+            console.log(123);
         }
+
+        $('body').on('click', '.view-all.selected', function (e) {
+            e.preventDefault();
+            blockUiLoader.unblockUi();
+            return;
+        });
     }
 
     return {
