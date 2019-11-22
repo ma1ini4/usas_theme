8f4d0773bb31594dace49a0ed6318eeb1017bd05
Alejandra Fernandez <alejandra@deplabs.com>
2019-11-22 02:59:13 +0000

    USASNGI-716: Implement Customer Conversion interface template

---
+++ scripts/modules/order/b2cOrders.js
@@ -0,0 +1,26 @@
+define(['jquery', 'hyprlive', 'modules/api','underscore'],
+  function($, Hypr, api, _ ){
+      var OrderDetailApi = {
+            getOrderDetail: function( params ) {
+              return this.performAction( '/custom/orderDetail', params);
+              },
+            updateCustomer: function( params ) {
+              return this.performAction( '/custom/updateCustomer', params);
+            },
+            getOrders: function(){
+              return this.performAction( '/custom/orders');
+            },
+            performAction: function (serviceurl,  params){
+              var apiData = require.mozuData('apicontext');
+              return $.ajax({
+                  url: serviceurl,
+                  headers: apiData.headers,
+                  method: 'POST',
+                  data: params
+                });
+              }
+        };
+      return {
+          OrderDetail: OrderDetailApi
+        };
+    });
